b3b431109beedf4b02966f9a11f430e27f271395
oleksandr.kulyna <oleksandr.kulyna@avenga.com>
2021-02-02 13:45:19 +0200

    check

--- server.js
+++ server.js
@@ -12,9 +12,24 @@ const server = express()
 
 const io = socketIO(server);
 
-io.on('connection', (socket) => {
-    console.log('Client connected');
-    socket.on('disconnect', () => console.log('Client disconnected'));
-});
+let openConnections = 0;
 
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+io.on('connection', (socket) => {
+    openConnections++;
+
+    io.emit('updated', {
+        openConnections
+    });
+
+    socket.on('ping', (data) => {
+        console.log('ping', data);
+    });
+
+    socket.on('disconnect', () => {
+        openConnections--;
+
+        io.emit('updated', {
+            openConnections
+        });
+    });
+});
